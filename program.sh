f5bf53abe0049ab264b11366f5bafa6261e236a0
Joel Whitesel <jwhitesel2@student.cscc.edu>
2020-11-17 23:39:31 -0500

    Sum the first 1000 even integers

--- program.sh
+++ program.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+# originally !/bin/sh
 
-#program.sh
-#Fill in the program described in the lab assignment below
-exit 0
+set -x
+#
+# Sum the first 1000 even numbers ...[zero is the first even counting number]
+#
+sum=0 	#Initialize the sum to zero
+
+for number in {0..999}
+do
+  #echo "number:" ${number} "2x:" $((2*${number}))
+  sum=$((${sum} + (2 * ${number})))
+done
+
+echo "sum:" ${sum}
+echo ${sum} > value.txt 
